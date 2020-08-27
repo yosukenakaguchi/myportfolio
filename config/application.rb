5ce9dbdba23b832804c4feb0fe8e822dd76bc807
中口陽介 <nakaguchiyousuke@nakaguchiyousukenoMacBook-Pro.local>
2020-08-27 10:18:32 +0900

    introduce_fb

--- config/application.rb
+++ config/application.rb
@@ -18,7 +18,6 @@ module Myportfolio
 
     config.generators do |g|
       g.test_framework :rspec,
-        fixtures: false,
         view_specs: false,
         helper_specs: false,
         routing_specs: false
