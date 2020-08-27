17de85ed5321720e72553a07648e7a6906731820
中口陽介 <nakaguchiyousuke@nakaguchiyousukenoMacBook-Pro.local>
2020-08-28 01:58:50 +0900

    Implement basic login

--- Gemfile
+++ Gemfile
@@ -7,6 +7,7 @@ ruby '2.7.1'
 gem 'rails', '~> 6.0.3', '>= 6.0.3.2'
 gem 'bootstrap-sass', '~> 3.4', '>= 3.4.1'
 gem 'jquery-rails', '~> 4.4'
+gem 'jquery-turbolinks'
 gem 'bcrypt', '~> 3.1', '>= 3.1.15'
 # Use mysql as the database for Active Record
 gem 'mysql2', '>= 0.5.2'
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  get '/signup', to: 'users#new'
-  root 'static_pages#home'
-  get '/about', to: 'static_pages#about'
+  root   'static_pages#home'
+  get    '/signup', to: 'users#new'
+  get    '/about',  to: 'static_pages#about'
+  get    '/login',  to: 'sessions#new'
+  post   '/login',  to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
   resources :users
 end
