2411da445ff2788b41f9ca2c80b3f3fd25ab13a6
YosukeNakaguchi <yosuke.nakaguchi.private@gmail.com>
2020-12-08 21:45:49 +0900

    edit_config_rspec

--- config/application.rb
+++ config/application.rb
@@ -16,6 +16,7 @@ module Myportfolio
 
     config.generators do |g|
       g.test_framework :rspec,
+        fixtures: false,
         view_specs: false,
         helper_specs: false,
         routing_specs: false
