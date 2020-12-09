063cb3020966aaaad84b498c736762976eaf6315
YosukeNakaguchi <yosuke.nakaguchi.private@gmail.com>
2020-12-09 22:09:39 +0900

    rspec_for_user_model

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,4 +1,40 @@
 FactoryBot.define do
   factory :user do
+    name { "Yosuke Nakaguchi" }
+    sequence(:email) { |n| "test#{n}@myportfolio.net" }
+    password { "password" }
+    password_confirmation {"password" }
+
+    #名前が無し
+    trait :name_nil do
+      name { nil }
+    end
+
+    #名前が51字以上
+    trait :name_more_than_50words do
+      name { "a" * 51 }
+    end
+
+    #メールアドレスがなし
+    trait :email_nil do
+      email { nil }
+    end
+
+    #メールアドレスが256文字以上
+    trait :email_more_than_255words do
+      email {"a" * 240 + "@myportfolio.net"}
+    end
+
+    #パスワードがなし
+    trait :no_password do
+      password { " " * 6 }
+      password_confirmation { " " * 6 }
+    end
+
+    #パスワードが5文字以下
+    trait :password_less_than_6words do
+      password{ "a" * 5 }
+      password_confirmation{ "a" * 5 }
+    end
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,80 +1,67 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  before do
-    @user = User.new(
-      name: "Yosuke Nakaguchi",
-      email: "yosuke.nakaguchi@gmail.com",
-      password: "foobar",
-      password_confirmation: "foobar"
-      )
+  it "名前がなければ無効な状態であること" do
+    user = FactoryBot.build(:user, :name_nil)
+    user.valid?
+    expect(user.errors[:name]).to include("can't be blank")
   end
 
-  it "is invalid without a name" do
-    @user.name = nil
-    @user.valid?
-    expect(@user.errors[:name]).to include("can't be blank")
+  it "ユーザー名が５１文字以上の場合は登録できないこと" do
+    user = FactoryBot.build(:user, :name_more_than_50words)
+    user.valid?
+    expect(user.errors[:name]).to include('is too long (maximum is 50 characters)')
   end
 
-  it "is invalid without a email" do
-    @user.email = nil
-    @user.valid?
-    expect(@user.errors[:email]).to include("can't be blank")
+  it "メールアドレスがなければ無効な状態であること" do
+    user = FactoryBot.build(:user, :email_nil)
+    user.valid?
+    expect(user.errors[:email]).to include("can't be blank")
   end
 
-  it "ユーザー名が５１文字以上の場合は登録できない" do
-    @user.name = "a" * 51
-    @user.valid?
-    expect(@user.errors[:name]).to include('is too long (maximum is 50 characters)')
+  it "メールアドレスが256文字以上の場合は登録できないこと" do
+    user = FactoryBot.build(:user, :email_more_than_255words)
+    user.valid?
+    expect(user.errors[:email]).to include('is too long (maximum is 255 characters)')
   end
 
-  it "emailが256文字以上の場合は登録できない" do
-    @user.email = "a" * 244 + "@example.com"
-    @user.valid?
-    expect(@user.errors[:email]).to include('is too long (maximum is 255 characters)')
+  it "メールアドレスが一意であること" do
+    user = FactoryBot.create(:user)
+    duplicate_user = user.dup
+    expect(duplicate_user).to be_invalid
   end
 
-  it "email validation should accept valid addresses" do
+  it "メールアドレスがvalid_addressesの場合は登録できないこと" do
     valid_addresses = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org
                          first.last@foo.jp alice+bob@baz.cn]
     valid_addresses.each do |valid_address|
-      @user.email = valid_address
-      expect(@user). to be_valid, "#{valid_address.inspect} should be invalid"
+      expect(FactoryBot.build(:user, email:valid_address)).to be_valid, "#{valid_address.inspect} should be invalid"
     end
   end
 
-  it "email validation should reject invalid addresses" do
+  it "メールアドレスがinvalid_addressesの場合は登録できないこと" do
     invalid_addresses = %w[user@example,com user_at_foo.org user.name@example.
                            foo@bar_baz.com foo@bar+baz.com foo@bar..com]
     invalid_addresses.each do |invalid_address|
-      @user.email = invalid_address
-      expect(@user). to be_invalid, "#{invalid_address.inspect} should be valid"
+      expect(FactoryBot.build(:user, email:invalid_addresses)).to be_invalid, "#{invalid_address.inspect} should be valid"
     end
   end
 
-  it "email addresses should be unique" do
-    duplicate_user = @user.dup
-    @user.save
-    expect(duplicate_user). to be_invalid
-  end
-
-  it "email addresses should be saved as lower-case" do
+  it "メールアドレスは小文字に変換されて登録されること" do
     mixed_case_email = "Foo@ExAMPle.CoM"
-    @user.email = mixed_case_email
-    @user.save
-    expect(mixed_case_email.downcase). to eq @user.reload.email
+    user = FactoryBot.create(:user, email:mixed_case_email)
+    expect(mixed_case_email.downcase). to eq user.reload.email
   end
 
-  it "is invalid without a password" do
-    @user.password = @user.password_confirmation =" " * 6
-    @user.valid?
-    expect(@user.errors[:password]).to include("can't be blank")
+  it "パスワードがなければ無効な状態であること" do
+    user = FactoryBot.build(:user, :no_password)
+    user.valid?
+    expect(user.errors[:password]).to include("can't be blank")
   end
 
-  it "パスワードが5文字以下の場合は登録できない" do
-    @user.password = @user.password_confirmation ="a" * 5
-    @user.valid?
-    expect(@user.errors[:password]).to include('is too short (minimum is 6 characters)')
+  it "パスワードが5文字以下の場合は登録できないこと" do
+    user = FactoryBot.build(:user, :password_less_than_6words)
+    user.valid?
+    expect(user.errors[:password]).to include('is too short (minimum is 6 characters)')
   end
-
 end
